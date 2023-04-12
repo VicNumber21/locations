0800a507a960dbe72c70090e0597e960ca4b9171
Victor Portnov <vic.portnov@gmail.com>
2023-04-12 23:10:50 +0300

    svc: add log markers to DbMigrator test

--- svc/src/it/scala/com/vportnov/locations/svc/DbMigratorTest.scala
+++ svc/src/it/scala/com/vportnov/locations/svc/DbMigratorTest.scala
@@ -6,6 +6,8 @@ import org.scalatest.matchers.should.Matchers._
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 
+import org.slf4j.LoggerFactory
+
 import org.flywaydb.core.api.FlywayException
 
 
@@ -14,6 +16,8 @@ class DbMigratorTest extends AnyDbSpec:
   info("As a developer I need to verify that initial and subsequent migrations pass sucessfully")
 
   "DbMigrator" should "apply all sql scripts on the first run" in {
+    logger.info("\n\nbegin first migration")
+
     Given("DbMigrator is running on clean database")
 
     When("migration is done")
@@ -21,9 +25,13 @@ class DbMigratorTest extends AnyDbSpec:
 
     Then("all sql scripts should be applied")
       scriptCount shouldBe 3
+
+    logger.info("\nend first migration\n")
   }
 
   it should "do nothing on the next run" in {
+    logger.info("\n\nbegin next migration")
+
     Given("DbMigrator is running on existing database")
 
     When("migration is done")
@@ -31,6 +39,8 @@ class DbMigratorTest extends AnyDbSpec:
 
     Then("no sql script should be applied")
       scriptCount shouldBe 0
+
+    logger.info("\nend next migration\n")
   }
 
   it should "throw in case of wrong database config" in {
@@ -42,3 +52,5 @@ class DbMigratorTest extends AnyDbSpec:
     Then("exception is thron")
       an[FlywayException] should be thrownBy DbMigrator.migrate[IO](brokenConfig).unsafeRunSync()
   }
+
+  private val logger = LoggerFactory.getLogger(classOf[DbMigratorTest])
